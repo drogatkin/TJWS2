2fd35e102a2fe1cc4e07755a998818222c9e2ad4
Dmitriy R <Dmitriy@metricstream.com>
2015-02-17 20:13:01 -0800

    removed redudant class

--- 1.x/src/rogatkin/wskt/SimpleServerContainer.java
+++ 1.x/src/rogatkin/wskt/SimpleServerContainer.java
@@ -25,7 +25,9 @@ import javax.websocket.server.ServerContainer;
 import javax.websocket.server.ServerEndpoint;
 import javax.websocket.server.ServerEndpointConfig;
 
-/** The container is created one per web application and one for default application
+/**
+ * The container is created one per web application and one for default
+ * application
  * 
  * @author Dmitriy
  *
@@ -34,15 +36,15 @@ public class SimpleServerContainer implements ServerContainer, ServletContextLis
 
 	HashMap<String, ServerEndpointConfig> endpoints;
 	SimpleProvider provider;
-	
+
 	HashSet<SimpleSession> sessions;
-	
+
 	ExecutorService asyncService;
-	
+
 	SimpleServerContainer(SimpleProvider simpleProvider) {
 		provider = simpleProvider;
 		endpoints = new HashMap<String, ServerEndpointConfig>();
-		sessions = new HashSet<SimpleSession>(); 
+		sessions = new HashSet<SimpleSession>();
 		asyncService = Executors.newSingleThreadExecutor();
 	}
 
@@ -50,28 +52,25 @@ public class SimpleServerContainer implements ServerContainer, ServletContextLis
 	public void addEndpoint(Class<?> arg0) throws DeploymentException {
 		if (arg0.isAssignableFrom(ServerEndpointConfig.class)) {
 			try {
-				addEndpoint((ServerEndpointConfig)arg0.newInstance());
+				addEndpoint((ServerEndpointConfig) arg0.newInstance());
 			} catch (InstantiationException e) {
-				throw new DeploymentException( "Error in deployment end point", e);
+				throw new DeploymentException("Error in deployment end point", e);
 			} catch (IllegalAccessException e) {
-				throw new DeploymentException( "Error in deployment end point", e);
+				throw new DeploymentException("Error in deployment end point", e);
 			}
 		} else {
 			ServerEndpoint sep = arg0.getAnnotation(ServerEndpoint.class);
-			String path = sep.value();
-			if (path == null || path.startsWith("/") == false)
-					throw new DeploymentException("Invalid path "+path);
-			//addEndpoint(ServerEndpointConfig.Builder.create(arg0, path).build());
-
-			addEndpoint(new SimpleServerEndpointConfig(arg0));
+			addEndpoint(ServerEndpointConfig.Builder.create(arg0, sep.value())
+					.subprotocols(Arrays.asList(sep.subprotocols())).encoders(Arrays.asList(sep.encoders()))
+					.decoders(Arrays.asList(sep.decoders())).build());
 		}
-		
+
 	}
 
 	@Override
 	public void addEndpoint(ServerEndpointConfig arg0) throws DeploymentException {
 		if (endpoints.containsKey(arg0.getPath()))
-			throw new DeploymentException("More than one end points use same path "+arg0.getPath());
+			throw new DeploymentException("More than one end points use same path " + arg0.getPath());
 		endpoints.put(arg0.getPath(), arg0);
 	}
 
@@ -130,55 +129,54 @@ public class SimpleServerContainer implements ServerContainer, ServletContextLis
 	@Override
 	public void setAsyncSendTimeout(long arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setDefaultMaxBinaryMessageBufferSize(int arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setDefaultMaxSessionIdleTimeout(long arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setDefaultMaxTextMessageBufferSize(int arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	void log(String msg, Object ...params) {
+
+	void log(String msg, Object... params) {
 		log(null, msg, params);
 	}
-	
-	void log(Throwable e, String msg, Object ...params) {
-		msg = "websocket : "+msg;
+
+	void log(Throwable e, String msg, Object... params) {
+		msg = "websocket : " + msg;
 		if (e == null)
 			if (params == null || params.length == 0)
 				provider.serve.log(msg);
 			else
 				provider.serve.log(String.format(msg, params));
+		else if (params == null || params.length == 0)
+			provider.serve.log(msg, e);
 		else
-			if (params == null || params.length == 0)
-				provider.serve.log(msg, e);
-			else
-				provider.serve.log(String.format(msg, params), e);
+			provider.serve.log(String.format(msg, params), e);
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
-		CloseReason cr = new CloseReason(CloseCodes.SERVICE_RESTART,"");
+		CloseReason cr = new CloseReason(CloseCodes.SERVICE_RESTART, "");
 		//ArrayList <SimpleSession> copyss = new ArrayList<SimpleSession>(sessions);
-		for(SimpleSession ss:new ArrayList<SimpleSession>(sessions))
+		for (SimpleSession ss : new ArrayList<SimpleSession>(sessions))
 			try {
 				System.err.printf("Closing session %s%n", ss);
 				ss.close(cr);
 			} catch (IOException e) {
-				
+
 			}
 		sessions.clear();
 		asyncService.shutdown();
@@ -186,18 +184,15 @@ public class SimpleServerContainer implements ServerContainer, ServletContextLis
 
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
-		
+
 	}
-	
+
 	void addSession(SimpleSession ss) {
 		sessions.add(ss);
 	}
-	
+
 	void removeSession(SimpleSession ss) {
 		sessions.remove(ss);
 	}
-	
-	
-
 
 }
