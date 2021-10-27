70e40a769bebcad83bf0ede63856b5905dfd9958
dmitriy <jaddressbook@gmail.com>
2021-10-26 17:25:49 -0700

    adoption JACORB, works on jdk 17

---
+++ 1.x/3rdparty/CORBA/java/javax/rmi/CORBA/Stub.java
@@ -0,0 +1,11 @@
+package javax.rmi.CORBA;
+
+import org.omg.CORBA_2_3.portable.ObjectImpl;
+
+/**
+ * JacOrb 3.9 surrogate 
+ * [Base class from which all RMI-IIOP stubs must inherit.]
+ */
+public abstract class Stub extends ObjectImpl implements java.io.Serializable
+{
+}
